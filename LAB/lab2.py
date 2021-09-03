bcc1511af167b725d37e160ee0016062e704a474
wtc <wtc@pop-os.localdomain>
2021-09-03 14:17:16 +0200

    feat: calculate the sum of diagonal elements

--- LAB/lab2.py
+++ LAB/lab2.py
@@ -11,3 +11,34 @@ def Hilbert(n):
 n = 3
 #test the function
 print(Hilbert(n))
+
+#declare variables
+sum_diagonal_elements = 0
+sum_upper_diagonal_elements = 0
+sum_lower_diagonal_elements = 0
+n = 1000
+
+#store the hilbert matrix where n is 1000
+matrix = Hilbert(n)
+
+#calculate sum for diagonal elements(red elements)
+for i in range(n):
+    sum_diagonal_elements += matrix[i,i]
+
+#calculate sum for upper diagonal elements(blue elements)
+for i in range(n):
+        if(i< n-1):
+            sum_upper_diagonal_elements += matrix[i,i+1]
+
+#calculate sum for lower diagonal elements(green elements)
+for i in range(n):
+    if(i < n-1):
+        sum_lower_diagonal_elements += matrix[i+1,i]
+
+#write function to display all the calculated sums
+def display_sum():
+    print("The sum for diagonal elements is: " + str(sum_diagonal_elements))
+    print("The sum for upper diagonal elements is: " + str(sum_upper_diagonal_elements))
+    print("The sum for lower diagonal elements is: " + str(sum_lower_diagonal_elements))
+
+display_sum()
